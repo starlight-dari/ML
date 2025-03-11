195b0c3a871ab1e62478b83916c5ace9156c3c7e
MinKyung Kim <mg09091234@gmail.com>
2025-03-11 13:08:54 +0900

    fix sh

--- download_and_run.sh
+++ download_and_run.sh
@@ -4,10 +4,14 @@
 FILE_ID="1Vf5vdnlW4Y2VurWzIK6o_Nxe0t4K7UQm"
 OUTPUT_FILE="sam_vit_b_01ec64.pth"
 
+FILE_ID2="1_EWkG2dUNIfkzrYfDPcOhP-osKxngX1V"
+OUTPUT_FILE2="table5_pidinet.pth"
+
 # 모델 파일 다운로드 (기존 파일이 없을 경우만 다운로드)
 if [ ! -f "$OUTPUT_FILE" ]; then
     echo "Downloading model checkpoint from Google Drive..."
     gdown --id $FILE_ID -O $OUTPUT_FILE
+    gdown --id $FILE_ID2 -O $OUTPUT_FILE2
 else
     echo "Model checkpoint already exists. Skipping download."
 fi
